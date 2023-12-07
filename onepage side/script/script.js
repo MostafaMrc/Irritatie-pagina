f31e8d5a0a2c24b032cf8ff1eaacb151d7b44260
TimSchoot <t.vanderschoot@student.alfa-college.nl>
2023-12-07 13:17:28 +0100

    popup2

---
+++ onepage side/script/script.js
@@ -0,0 +1,14 @@
+function showPopup() {
+    document.getElementById('overlay').style.display = 'block';
+    document.getElementById('popup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('overlay').style.display = 'none';
+    document.getElementById('popup').style.display = 'none';
+}
+
+window.onload = function () {
+    showPopup();
+    setInterval(showPopup, 10000);
+};
