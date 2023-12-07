85c2adfd0331706c4c9209b5fc90248f2bf13ebd
MostafaMrc <145435670+MostafaMrc@users.noreply.github.com>
2023-12-07 13:43:56 +0100

    rick roll working

--- onepage side/javascript/video.js
+++ onepage side/javascript/video.js
@@ -1,9 +1,18 @@
-document.getElementById('photo-iphone').addEventListener('click', function() {
-    // Hide the image
-    this.style.display = 'none';
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the video element
+    var video = document.getElementById('VideoRoll');
 
-    // Show and play the video
-    let video = document.getElementById('videoPlayer');
-    video.style.display = 'block';
-    video.play();
-  });
+    // Add click event listener to the image element
+    document.getElementById('photo-iphone').addEventListener('click', function() {
+        // Show the video
+        video.style.display = 'block';
+        
+        // Toggle the mute state
+        video.muted = !video.muted;
+
+        // If video is unmuted, play it
+        if (!video.muted) {
+            video.play();
+        }
+    });
+});
