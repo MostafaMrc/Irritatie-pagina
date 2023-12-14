9d73d797967d6846f1ef6c17968bca56db637748
TimSchoot <t.vanderschoot@student.alfa-college.nl>
2023-12-14 10:12:58 +0100

    alle JS in 1 file

--- onepage side/script/script.js
+++ onepage side/script/script.js
@@ -12,4 +12,21 @@ window.onload = function () {
     showPopup();
     setInterval(showPopup, 10000);
 };
-
+document.addEventListener('DOMContentLoaded', function() {
+    var video = document.getElementById('videoroll');
+    document.getElementById('foto-iphone').addEventListener('click', function() {
+        video.style.display = 'block';
+        video.muted = !video.muted;
+        if (!video.muted) {
+            video.play();
+        }
+    });
+});
+function showImage() {
+    var image = document.querySelector('.showImage');
+    if (image.style.display === 'none') {
+        image.style.display = 'block';
+    } else {
+        image.style.display = 'none';
+    }
+}
